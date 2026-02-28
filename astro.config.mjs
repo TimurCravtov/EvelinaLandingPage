5a6d9011deff7eda82f5779fd3eeab6a1998b3c9
Timur Cravtov <timurcravtov04@gmail.com>
2026-02-28 14:51:42 +0200

    ci/cd: update deply script

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://timurcravtov.github.io/EvelinaLandingPage',
+  base: '/EvelinaLandingPage',
 });
